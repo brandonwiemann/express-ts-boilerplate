c633330bdda5ff2a36bf570560c2242761a711f6
Brandon Wiemann <me@brandonwiemann.com>
2020-10-22 12:06:34 -0500

    adds migrations

--- ormconfig.js
+++ ormconfig.js
@@ -1,4 +1,8 @@
-const path = require('path');
+const baseDir = 'src/data';
+const entitiesDir = baseDir + 'entities';
+const factoriesDir = baseDir + 'factories';
+const migrationsDir = baseDir + 'migrations';
+const seedsDir = baseDir + 'seeds';
 
 module.exports = {
     type: 'mysql',
@@ -7,10 +11,16 @@ module.exports = {
     username: process.env.SQL_USER,
     password: process.env.SQL_PASSWORD,
     database: process.env.SQL_DB_NAME,
-    entities: [
-        path.join(__dirname, 'src/data/entity/*.ts')
-    ],
-    synchronize: true,
-    seeds: ['src/data/seeds/**/*.ts'],
-    factories: ['src/data/factories/**/*.ts']
+    synchronize: process.env !== 'production',
+    entities: [`${entitiesDir}/**/*.ts`],
+    seeds: [`${seedsDir}/**/*.ts`],
+    factories: [`${factoriesDir}/**/*.ts`],
+    migrationsTableName: 'db_migrations',
+    migrations: [`${migrationsDir}/*.ts`],
+    cli: {
+        entitiesDir,
+        factoriesDir,
+        migrationsDir,
+        seedsDir
+    }
 };
